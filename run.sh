25421a75b203aa2e81ca9df60ce0cbf2af0fef6d
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-07 06:33:21 +0530

    Update script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 source /data/secrets
 
-if [ -z "$CART_ENDPOINT" -o -z "$DB_HOST" ]; then
+if [ -z "$CART_ENDPOINT" -o -z "$DB_HOST" -o -z "${license_key}" ]; then
   echo Environment Variables is mising
   exit 1
 fi
 
-java -javaagent:newrelic/newrelic.jar -jar /app/shipping.jar
+java -javaagent:newrelic/newrelic.jar -Dnewrelic.config.license_key=${license_key} -jar /app/shipping.jar
